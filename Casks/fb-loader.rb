afe813009d3ca5c19c023c54d327094bcf4947cd
Alexey Orshanski <alexey.orshanski@gmail.com>
2026-05-02 12:56:20 +0300

    fb-loader 1.1

--- Casks/fb-loader.rb
+++ Casks/fb-loader.rb
@@ -1,6 +1,6 @@
 cask "fb-loader" do
-  version "1.0"
-  sha256 "d6b0204248212928b0c2697309a09216e16eb13d09a043090ce3d46a489af02e"
+  version "1.1"
+  sha256 "10ee20da5d994118a99977e58f902cb5614c8a9b8ea2304af3c51a5ae1928a39"
 
   url "https://github.com/Orshanski/fb-loader/releases/download/v#{version}/FB.Loader-#{version}.dmg"
   name "FB Loader"
